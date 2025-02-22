adfaf10f1c4d9c423bf976d837e2479e1aa80e07
rawatcode <rawatcode@gmail.com>
2025-02-22 23:10:25 +0530

    fixed port issues

--- backend/main.py
+++ backend/main.py
@@ -146,4 +146,4 @@ if __name__ == "__main__":
 
     # Start FastAPI server
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
